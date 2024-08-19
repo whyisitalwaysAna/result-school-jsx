ef452af45e5844252f29c147f2ba9a9997488a9b
Anna Safonova <annasafonova.info@gmail.com>
2024-08-19 16:22:52 +0300

    feat: add year after react link, add comments

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ * ? Мне кажется здесь везде декларативный стиль, верно-ли это?
+ * ? Мы экспортируем константу App, в целом, мы понимаем что она делает - это приложение.
+ * ? Внутри приложения описываем HTML, весь HTML декларативный.
+ * ? В теге time мы обращаемся к сегодняшней дате и явно просим текущий год.
+ */
+
 export const App = () => {
 	return (
 		<div className='App'>
@@ -17,9 +24,8 @@ export const App = () => {
 				>
 					Learn React
 				</a>
+				<time>{new Date().getFullYear()}</time>
 			</header>
 		</div>
 	);
 };
-
-export default App;
