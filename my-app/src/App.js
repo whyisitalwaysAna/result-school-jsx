a25e78115c8a8dbe5ec3a99dfe6eb12dcdec6398
Anna Safonova <annasafonova.info@gmail.com>
2024-08-19 16:39:24 +0300

    style: rewrite jsx to js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-
-/**
- * ? Мне кажется здесь везде декларативный стиль, верно-ли это?
- * ? Мы экспортируем константу App, в целом, мы понимаем что она делает - это приложение.
- * ? Внутри приложения описываем HTML, весь HTML декларативный.
- * ? В теге time мы обращаемся к сегодняшней дате и явно просим текущий год.
- */
+import { createElement } from 'react';
 
 export const App = () => {
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-				<time>{new Date().getFullYear()}</time>
-			</header>
-		</div>
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js '),
+				'and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('time', null, new Date().getFullYear()),
+		),
 	);
 };
