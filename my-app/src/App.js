76588eba03f57eca729e6d541287d069bf1f4be9
Anna Safonova <annasafonova.info@gmail.com>
2024-08-28 13:51:17 +0300

    fix: fix sample

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import { createElement } from 'react';
 
 export const App = () => {
-	return createElement(
-		'div',
-		{ className: 'App' },
-		createElement(
-			'header',
-			{ className: 'App-header' },
-			createElement('img', {
-				src: logo,
-				className: 'App-logo',
-				alt: 'logo',
-			}),
-			createElement(
-				'p',
-				null,
-				'Edit ',
-				createElement('code', null, 'src/App.js '),
-				'and save to reload.',
-			),
-			createElement(
-				'a',
-				{
-					className: 'App-link',
-					href: 'https://reactjs.org',
-					target: '_blank',
-					rel: 'noopener noreferrer',
-				},
-				'Learn React',
-			),
-			createElement('time', null, new Date().getFullYear()),
-		),
-	);
+	return <div>Hello</div>;
 };
